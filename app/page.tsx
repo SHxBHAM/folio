a45ebd83faf173d63d6c22e3322004dda5163b2c
SHXBHAM <shubhammishra41880@gmail.com>
2025-06-15 22:57:11 +0530

    chore: fix unscaped char error

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,26 @@ import { Meteors } from "@/components/magicui/meteors";
 import { ThemeProvider } from "@/components/themeprovider";
 import { ModeToggle } from "@/components/mode-toggle";
 
-export default function home() {
+export default function Home() {
   return (
-      <ThemeProvider
+    <ThemeProvider
       attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-            >
-    <div className="relative flex h-[100vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg border">
-      <Meteors number={30} />
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Yo wssup! it's me shubham.
-      </span>
-      <span className=" mt-4 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-lg font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        The portfolio is under construction, but you can still check out my shit on <a className="underline"href="https://github.com/SHxBHAM">github</a>   <a className="underline"href="https://www.linkedin.com/in/shubhxm/">Linkedin</a>
-      </span>
-      <div><ModeToggle></ModeToggle></div>
-    </div>
-      </ThemeProvider>
-
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="relative flex h-[100vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg border">
+        <Meteors number={30} />
+        <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+          {"Yo wssup! it's me shubham."}
+        </span>
+        <span className="mt-4 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-lg font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+          The portfolio is under construction, but you can still check out my shit on{" "}
+          <a className="underline" href="https://github.com/SHxBHAM">github</a>{" "}
+          <a className="underline" href="https://www.linkedin.com/in/shubhxm/">Linkedin</a>
+        </span>
+        <div><ModeToggle /></div>
+      </div>
+    </ThemeProvider>
   );
 }
